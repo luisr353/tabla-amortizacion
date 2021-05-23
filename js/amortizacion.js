94dff00650c101ecfa64c1b29eb1df3cae33e317
LuisR <luisrodriguezacosta@gmail.com>
2021-05-23 18:24:43 -0500

    Add files via upload
    Se corrigió formula de anticipada

--- js/amortizacion.js
+++ js/amortizacion.js
@@ -9,10 +9,26 @@ const btnCalcular = document.getElementById('btnCalcular');
 const llenarTabla = document.querySelector('#lista-tabla tbody');
 
 btnCalcular.addEventListener('click', () => {
-    calcularCuota(Number(monto.value), Number(tasa.value), Number(tiempo.value), seguro.value);
+
+    console.log(modalidad.value);
+
+    switch (modalidad.value) {
+        case  "1" :
+             calcularCuotaAmortizaciónVencidas(Number(monto.value), Number(tasa.value), Number(tiempo.value), seguro.value);
+        break;
+        
+        case "2" :
+        calcularCuotaAnticipadas(Number(monto.value), Number(tasa.value), Number(tiempo.value), seguro.value);
+        break;
+    
+        default:
+        break;
+    }
+   
 })
 
-function calcularCuota(monto, tasa, tiempo, seguro){
+function calcularCuotaAmortizaciónVencidas(monto, tasa, tiempo, seguro){
+
 
     while(llenarTabla.firstChild){
         llenarTabla.removeChild(llenarTabla.firstChild);
@@ -72,16 +88,77 @@ function calcularCuota(monto, tasa, tiempo, seguro){
     }
 }
 
-/*
-<th>Fecha</th>
-                            <th>Interese</th>
-                            <th>Cuota sin seguro</th>
-                            <th>Seguro</th>
-                            <th>Capital</th>
-                            <th>Cuota + seguro</th>
-                            <th>Amortización</th>
-                            <th>Saldo</th>
+function calcularCuotaAnticipadas(monto, tasa, tiempo, seguro){
 
-                            <td>${pagoCapital.toFixed(2)}</td>
-                            <td>${pagoInteres.toFixed(2)}</td>
-*/
+    while(llenarTabla.firstChild){
+        llenarTabla.removeChild(llenarTabla.firstChild);
+    }
+
+
+    const formatterPeso = new Intl.NumberFormat('es-CO', {
+        style: 'currency',
+        currency: 'COP',
+        minimumFractionDigits: 0
+      })
+
+
+    var tasa = tasa /100;
+    var mes = 1 ;
+
+    /*let fechas = [];
+    let fechaActual = Date.now();
+    let mes_actual = moment(fechaActual);
+    mes_actual.add(1, 'month');*/
+     
+    let pagoInteres = 0, pagoCapital = 0, cuota = 0;
+
+    //=monto* ((TASA*(1+TASA)^tiempo)/(((1+TASA)^tiempo)-1))
+
+    //(MONTO/(1+TASA)*((TASA*(1+TASA)^PLAZO)/((1+TASA)^PLAZO-1)))
+
+    //debugger
+
+    ej = monto/(1+tasa);
+    ej2 = tasa * (Math.pow((1+tasa), tiempo));
+    ej3 = Math.pow((1 + tasa), tiempo) - 1;
+
+    //(MONTO/(1+TASA)*((TASA*(1+TASA)^PLAZO)/((1+TASA)^PLAZO-1)))
+    cuota = (monto / (1 + tasa) * ((tasa * (Math.pow((1+tasa), tiempo))) / ((Math.pow((1+tasa), tiempo)-1))));
+    
+    console.log(ej);
+    console.log(ej2);
+    console.log(ej3);
+    console.log(cuota);
+    
+    for(let i = 1; i <= tiempo; i++) {
+
+        pagoInteres = parseFloat(monto*(tasa));
+        pagoCapital = parseFloat(cuota - pagoInteres);
+        monto = parseFloat(monto-pagoCapital);
+        seguro = parseFloat(seguro);
+        cuotaSeguro =  parseFloat(cuota + seguro);
+        amortizacion = parseFloat(cuota - pagoInteres);
+        saldo = parseFloat(amortizacion);
+        
+
+  //debugger
+        /*//Formato fechas
+        fechas[i] = mes_actual.format('DD-MM-YYYY');
+        mes_actual.add(1, 'month');*/
+
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${mes++}</td>
+            <td>${formatterPeso.format(monto)}</td>
+            <td>${formatterPeso.format(pagoInteres)}</td>
+            <td>${formatterPeso.format(cuota.toFixed(2))}</td>
+            <td>${formatterPeso.format(seguro)}</td>
+            <td>${formatterPeso.format(cuotaSeguro)}</td>
+            <td>${formatterPeso.format(amortizacion)}</td>
+            <td>${formatterPeso.format(monto)}</td>
+        `;
+        llenarTabla.appendChild(row)
+    }
+}
+
+
